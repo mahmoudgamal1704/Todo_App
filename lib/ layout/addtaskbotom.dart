4eb306c54a4cc5b7b4ce321800742605fe74a4e6
mahmoud <mahmoudgamal1704@gmail.com>
2022-12-19 18:06:16 +0200

    fix overflow for bottomSheet

--- lib/ layout/addtaskbotom.dart
+++ lib/ layout/addtaskbotom.dart
@@ -27,105 +27,107 @@ class AddTaskBottom extends StatelessWidget {
         return Container(
           width: double.infinity,
           margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                AppLocalizations.of(context)!.addtitle,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Form(
-                  key: frmKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: prov.titleController,
-                        validator: (value) {
-                          if (value?.trim() == "" || value == null) {
-                            return 'Please Enter your title';
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                            label: Text(AppLocalizations.of(context)!.taskname),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(color: Primarycolor)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(color: Primarycolor))),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        controller: prov.discrpController,
-                        validator: (value) {
-                          if (value?.trim() == "" || value == null) {
-                            return 'Please Enter your title';
-                          }
-                          return null;
-                        },
-                        maxLines: 3,
-                        decoration: InputDecoration(
-                            label: Text(AppLocalizations.of(context)!.taskdesc),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(color: Primarycolor)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(color: Primarycolor))),
-                      ),
-                    ],
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                AppLocalizations.of(context)!.datetitle,
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              InkWell(
-                onTap: () {
-                  prov.SelectDate(context);
-                },
-                child: Text(
-                  '${DateFormat.yMMMEd(AppLocalizations.of(context)!.datelang).format(prov.selectedDate)}',
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.addtitle,
                   textAlign: TextAlign.center,
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (frmKey.currentState!.validate()) {
-                    Task task = Task(
-                        title: prov.titleController.text,
-                        description: prov.discrpController.text,
-                        date: prov.selectedDate.microsecondsSinceEpoch);
-                    addTaskToFireStore(task).then((value) {
-                      taskprov.getTaskfromFirestore(taskprov.currentDate);
-                      Navigator.of(context).pop();
-                    });
-                  }
-                },
-                child: Text(AppLocalizations.of(context)!.addbutt,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline1
-                        ?.copyWith(fontSize: 20)),
-              )
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Form(
+                    key: frmKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: prov.titleController,
+                          validator: (value) {
+                            if (value?.trim() == "" || value == null) {
+                              return 'Please Enter your title';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                              label: Text(AppLocalizations.of(context)!.taskname),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(color: Primarycolor)),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(color: Primarycolor))),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          controller: prov.discrpController,
+                          validator: (value) {
+                            if (value?.trim() == "" || value == null) {
+                              return 'Please Enter your title';
+                            }
+                            return null;
+                          },
+                          maxLines: 3,
+                          decoration: InputDecoration(
+                              label: Text(AppLocalizations.of(context)!.taskdesc),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(color: Primarycolor)),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(color: Primarycolor))),
+                        ),
+                      ],
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  AppLocalizations.of(context)!.datetitle,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    prov.SelectDate(context);
+                  },
+                  child: Text(
+                    '${DateFormat.yMMMEd(AppLocalizations.of(context)!.datelang).format(prov.selectedDate)}',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (frmKey.currentState!.validate()) {
+                      Task task = Task(
+                          title: prov.titleController.text,
+                          description: prov.discrpController.text,
+                          date: prov.selectedDate.microsecondsSinceEpoch);
+                      addTaskToFireStore(task).then((value) {
+                        taskprov.getTaskfromFirestore(taskprov.currentDate);
+                        Navigator.of(context).pop();
+                      });
+                    }
+                  },
+                  child: Text(AppLocalizations.of(context)!.addbutt,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline1
+                          ?.copyWith(fontSize: 20)),
+                )
+              ],
+            ),
           ),
         );
       },
